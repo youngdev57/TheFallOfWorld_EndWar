cf7f48f8ce2a50ede89cd0d6382eb5ac6da25be0
wlfmarnltls <zkwks112@gmail.com>
2020-04-29 17:49:25 +0900

    스킬 업데이트

--- EndWar/Assets/Scripts/Lee/Player/PlayerCtrl.cs
+++ EndWar/Assets/Scripts/Lee/Player/PlayerCtrl.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    [PunRPC]
     public void GetDamage()
     {
         Debug.Log("아프다~~~~~ 행~복~~해~~~줘~~어~~~~" + this.gameObject.name);
