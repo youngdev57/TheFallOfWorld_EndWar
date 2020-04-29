ff4761953a47028068cc890de8dfef41dbb9eab0
rpdst <cjfdls20000@naver.com>
2020-04-29 16:33:47 +0900

    K2 - 작업 폴더 생성/ 포톤 테스트 버전 임포트
    Environment(맵 환경 제작물)
    Particle(파티클 관련 재료)
    Prop(사물/오브젝트), Skybox(스카이박스)
    Photon(포톤 패키지 *중요*)
    Resources(포톤은 프리팹 생성을 Resources 폴더에서만 가능)
    Terrain(터레인 폴더)
    
    위 7개 폴더 생성
    
    포톤 서버에 접속하여 서로의 캐릭터를 확인 할 수 있음
    움직임, 회전 모두 정상 작동

---
+++ EndWar/Assets/Scenes/Kang/PhotonTest/PlayerName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerName : MonoBehaviourPunCallbacks
+{
+    public Text nickname;
+
+    void Start()
+    {
+        nickname.text = photonView.Owner.NickName;
+    }
+}
