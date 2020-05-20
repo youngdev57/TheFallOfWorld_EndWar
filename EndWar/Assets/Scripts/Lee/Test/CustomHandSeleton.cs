705fc626cdca4e90e0ee10410d30f8af2e6d965b
wlfmarnltls <zkwks112@gmail.com>
2020-05-20 19:50:20 +0900

    no message

---
+++ EndWar/Assets/Scripts/Lee/Test/CustomHandSeleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Valve.VR;
+
+public class CustomHandSeleton : SteamVR_Behaviour_Skeleton
+{
+    PhotonView myPv;
+  
+    new void Awake()
+    {
+        myPv = GetComponent<PhotonView>();
+        if (myPv.IsMine)
+            base.Awake();
+    }
+
+    new void OnEnable()
+    {
+        if (myPv.IsMine)
+            base.OnEnable();
+    }
+
+    new void OnDisable()
+    {
+        if (myPv.IsMine)
+            base.OnDisable();
+    }
+}
