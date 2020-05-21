b138833c08598205d1861e8caec79fcd186c2850
wlfmarnltls <zkwks112@gmail.com>
2020-05-21 11:30:48 +0900

    no message

--- EndWar/Assets/Scripts/Lee/Test/CustomHandSeleton.cs
+++ EndWar/Assets/Scripts/Lee/Test/CustomHandSeleton.cs
@@ -26,4 +26,77 @@ public class CustomHandSeleton : SteamVR_Behaviour_Skeleton
         if (myPv.IsMine)
             base.OnDisable();
     }
+
+    public void UseGetBonePositions()
+    {
+        myPv.RPC("C_GetBonePositions", RpcTarget.AllBuffered, null);
+    }
+
+    public void UseGetBoneRotations()
+    {
+        myPv.RPC("C_GetBoneRotations", RpcTarget.AllBuffered, null);
+    }
+
+    [PunRPC]
+    public Quaternion[] C_GetBoneRotations()
+    {
+        if (skeletonAvailable)
+        {
+            Quaternion[] rawSkeleton = skeletonAction.GetBoneRotations();
+            if (mirroring == MirrorType.LeftToRight || mirroring == MirrorType.RightToLeft)
+            {
+                for (int boneIndex = 0; boneIndex < rawSkeleton.Length; boneIndex++)
+                {
+                    rawSkeleton[boneIndex] = MirrorRotation(boneIndex, rawSkeleton[boneIndex]);
+                }
+            }
+
+            return rawSkeleton;
+
+        }
+        else
+        {
+            //fallback to getting skeleton pose from skeletonPoser
+            if (fallbackPoser != null)
+            {
+                return fallbackPoser.GetBlendedPose(skeletonAction, inputSource).boneRotations;
+            }
+            else
+            {
+                Debug.LogError("Skeleton Action is not bound, and you have not provided a fallback SkeletonPoser. Please create one to drive hand animation when no skeleton data is available.", this);
+                return null;
+            }
+        }
+    }    
+
+    [PunRPC]
+    public Vector3[] C_GetBonePositions()
+    {
+        if (skeletonAvailable)
+        {
+            Vector3[] rawSkeleton = skeletonAction.GetBonePositions();
+            if (mirroring == MirrorType.LeftToRight || mirroring == MirrorType.RightToLeft)
+            {
+                for (int boneIndex = 0; boneIndex < rawSkeleton.Length; boneIndex++)
+                {
+                    rawSkeleton[boneIndex] = MirrorPosition(boneIndex, rawSkeleton[boneIndex]);
+                }
+            }
+
+            return rawSkeleton;
+        }
+        else
+        {
+            //fallback to getting skeleton pose from skeletonPoser
+            if (fallbackPoser != null)
+            {
+                return fallbackPoser.GetBlendedPose(skeletonAction, inputSource).bonePositions;
+            }
+            else
+            {
+                Debug.LogError("Skeleton Action is not bound, and you have not provided a fallback SkeletonPoser. Please create one to drive hand animation when no skeleton data is available.", this);
+                return null;
+            }
+        }
+    }
 }
