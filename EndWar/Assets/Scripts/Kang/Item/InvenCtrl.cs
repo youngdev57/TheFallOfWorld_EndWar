406b5b32a3e4ed8a13748713cf0a3e54cad36022
rpdst <cjfdls20000@naver.com>
2020-05-26 23:44:24 +0900

    K9 - 인벤토리 프로토타입 제작

---
+++ EndWar/Assets/Scripts/Kang/Item/InvenCtrl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvenCtrl : MonoBehaviour
+{
+    public GameObject inventoryCanvas;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryCanvas.SetActive(!inventoryCanvas.activeSelf);
+        }
+    }
+}
