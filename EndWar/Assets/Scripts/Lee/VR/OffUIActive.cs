b0a419b5a26a5a0df5580c860cf4c3238aac4bc5
wlfmarnltls <zkwks112@gmail.com>
2020-06-11 22:10:19 +0900

    화려한 조명이 나를 감싸네~~

---
+++ EndWar/Assets/Scripts/Lee/VR/OffUIActive.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class OffUIActive : MonoBehaviourPun
+{
+    public PhotonView player;
+
+    void Start()
+    {
+        if (!player.IsMine)
+            gameObject.SetActive(false);
+    }
+}
