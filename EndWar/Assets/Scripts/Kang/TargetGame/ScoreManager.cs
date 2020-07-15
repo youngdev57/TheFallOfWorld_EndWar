822523c8cf4d3265a942e87cce8bcc30d4ad4c7b
rpdst <cjfdls50@gmail.com>
2020-07-15 16:47:15 +0900

    KMS5 - 사격 게임 맵 제작, 총 애니메이션 수정 (상재)

---
+++ EndWar/Assets/Scripts/Kang/TargetGame/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static ScoreManager instance;
+    public static ScoreManager GetInstance()
+    {
+        return instance;
+    }
+
+    public TextMeshProUGUI score_text;
+
+    [SerializeField]
+    int score = 0;
+
+    void Awake()
+    {
+        instance = this;
+
+        score_text.text = score.ToString();
+    }
+
+    public void AddScore(int n)
+    {
+        if (score + n < 0)
+            score = 0;
+        else
+            score += n;
+
+        score_text.text = score.ToString();
+    }
+
+    public int GetScore()
+    {
+        return score;
+    }
+}
---
+++ EndWar/Assets/Scripts/Kang/TargetGame/Target_Receiver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_Receiver : MonoBehaviour
+{
+    public Target_Sender sender;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Contains("Respawn"))
+        {
+            ScoreManager.GetInstance().AddScore(-1);
+            sender.Restore(other.gameObject);
+            Debug.Log("TriggerEnter");
+        }
+    }
+}
